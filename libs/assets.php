8c907d0c4b1a7086806f0709550165941a5ec326
Kevin Kaske <kevinkaske@gmail.com>
2016-02-01 15:47:31 -0600

    Changes to deploy

--- libs/assets.php
+++ libs/assets.php
@@ -4,7 +4,7 @@ function cssTags(){
 	$css_tags = '';
 
 	if($config['env'] == 'prod'){
-		$css_tags = '<link rel="stylesheet" type="text/css" href="'.$config['address'].'/assets/css/master'.$deploy_id.'.css" media="screen">';
+		$css_tags = '<link rel="stylesheet" type="text/css" href="'.$config['address'].'/assets/css/master'.$css_md5.'.css" media="screen">';
 	}else{
 		foreach($css_files as $css_file) {
 			$css_tags = $css_tags.'<link rel="stylesheet" type="text/css" href="'.$config['address'].'/assets/css/'.$css_file.'.css" media="screen">'."\n";
@@ -23,11 +23,11 @@ function cssTagsForDeploy(){
 }
 
 function jsTags(){
-	global $config, $js_files, $css_md5;
+	global $config, $js_files, $js_md5;
 	$js_tags = '';
 
 	if($config['env'] == 'prod'){
-		$js_tags = '<script type="text/javascript" src="'.$config['address'].'/assets/js/master'.$deploy_id.'.js"></script>';
+		$js_tags = '<script type="text/javascript" src="'.$config['address'].'/assets/js/master'.$js_md5.'.js"></script>';
 	}else{
 		foreach($js_files as $js_file) {
 			$js_tags = $js_tags.'<script type="text/javascript" src="'.$config['address'].'/assets/js/'.$js_file.'.js"></script>'."\n";
