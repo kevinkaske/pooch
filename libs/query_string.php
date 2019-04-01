0dfc9ba6122b313d30b36ad7be6db0da0cf9d00b
kevinkaske <kevinkaske@gmail.com>
2019-04-01 15:56:40 -0500

    Change query_string.php to not throw warning when running in CLI

--- libs/query_string.php
+++ libs/query_string.php
@@ -8,21 +8,22 @@ function getQueryString(){
 	global $query_string;
 	$query_string = array();
 	$new_query_string = array();
-	if(strpos($_SERVER['REQUEST_URI'], '?')){
-		$query_string = explode('?', $_SERVER['REQUEST_URI']);
+	if(isset($_SERVER) && isset($_SERVER['REQUEST_URI'])){
+		if(strpos($_SERVER['REQUEST_URI'], '?')){
+			$query_string = explode('?', $_SERVER['REQUEST_URI']);
 
-		if(strpos($query_string[1], '&')){
-			$query_string = explode('&', $query_string[1]);
-			foreach ($query_string as $value) {
-				$value_array = explode('=', $value);
+			if(strpos($query_string[1], '&')){
+				$query_string = explode('&', $query_string[1]);
+				foreach ($query_string as $value) {
+					$value_array = explode('=', $value);
+					$new_query_string[urldecode($value_array[0])] = urldecode($value_array[1]);
+				}
+				$query_string = $new_query_string;
+			}else{
+				$value_array = explode('=', $query_string[1]);
 				$new_query_string[urldecode($value_array[0])] = urldecode($value_array[1]);
 			}
-			$query_string = $new_query_string;
-		}else{
-			$value_array = explode('=', $query_string[1]);
-			$new_query_string[urldecode($value_array[0])] = urldecode($value_array[1]);
 		}
-	}
 
 	$query_string = $new_query_string;
 }
