a8d0cf11023fdae8636cb59f551ae79a42712512
Kevin Kaske <kevinkaske@gmail.com>
2024-11-05 18:19:37 -0600

    Update application_controller.php

--- setup/base/controllers/application_controller.php
+++ setup/base/controllers/application_controller.php
@@ -1,7 +1,7 @@
 <?php
 class ApplicationController extends Controller{
-	function __construct($controller, $action) {
-		parent::__construct($controller, $action);
+	function __construct($controller, $action, $id) {
+		parent::__construct($controller, $action, $id);
 		
 		
 		//Put in code that should be run in every request here
